04f2f579bd0ed223cff02072034a99ee697ac1fc
Alena <alena.karpov@gmail.com>
2024-07-17 11:36:16 +0100

    feat: js: add slider functionality for the  first page

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
 	'env': {
 		'browser': true,
 		'commonjs': true,
-		'es2021': true
+		// 'es2021': true
+		'es6': true
 	},
 	// 'extends': 'eslint:recommended',
 	'parserOptions': {
-		'ecmaVersion': 'latest'
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
 	},
 	'rules': {
 		'indent': [
