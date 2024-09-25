e052c2bda3e5aa7468589d126cdf35af46167f47
Alena <alena.karpov@gmail.com>
2024-09-25 14:01:28 +0100

    doc: add gitattributes

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,46 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const browsersync = require('browser-sync');
 
-const dist = './dist/';
-// const dist = '/Applications/MAMP/htdocs/3_JS_OOP_loan'; // Ссылка на вашу папку на сервере
+// const dist = './dist/';
+const dist = '/Applications/MAMP/htdocs/3_JS_OOP_loan'; // Ссылка на вашу папку на сервере
 
 gulp.task('copy-html', () => {
-	return gulp.src('./src/*.html')
+	return gulp
+		.src('./src/*.html')
 		.pipe(gulp.dest(dist))
 		.pipe(browsersync.stream());
 });
 
 gulp.task('build-js', () => {
-	return gulp.src('./src/js/main.js')
-		.pipe(webpack({
-			mode: 'development',
-			output: {
-				filename: 'script.js'
-			},
-			watch: false,
-			devtool: 'source-map',
-			module: {
-				rules: [
-					{
-						test: /\.m?js$/,
-						exclude: /(node_modules|bower_components)/,
-						use: {
-							loader: 'babel-loader',
-							options: {
-								presets: [['@babel/preset-env', {
-									debug: true,
-									corejs: 3,
-									useBuiltIns: 'usage'
-								}]]
+	return gulp
+		.src('./src/js/main.js')
+		.pipe(
+			webpack({
+				mode: 'development',
+				output: {
+					filename: 'script.js'
+				},
+				watch: false,
+				devtool: 'source-map',
+				module: {
+					rules: [
+						{
+							test: /\.m?js$/,
+							exclude: /(node_modules|bower_components)/,
+							use: {
+								loader: 'babel-loader',
+								options: {
+									presets: [['@babel/preset-env', {
+										debug: true,
+										corejs: 3,
+										useBuiltIns: 'usage'
+									}]]
+								}
 							}
 						}
-					}
-				]
-			}
-		}))
+					]
+				}
+			}))
 		.pipe(gulp.dest(dist))
 		.on('end', browsersync.reload);
 });
@@ -71,7 +74,8 @@ gulp.task('watch', () => {
 gulp.task('build', gulp.parallel('copy-html', 'copy-assets', 'build-js'));
 
 gulp.task('build-prod-js', () => {
-	return gulp.src('./src/js/main.js')
+	return gulp
+		.src('./src/js/main.js')
 		.pipe(webpack({
 			mode: 'production',
 			output: {
